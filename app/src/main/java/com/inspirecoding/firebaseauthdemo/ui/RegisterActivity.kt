9c0ed40f21310667804f14495e5b64701e9daf03
inspire-coding <+PaZ+PeTi+88113+>
2020-02-20 21:21:05 +0100

    The UI of register and login

---
+++ app/src/main/java/com/inspirecoding/firebaseauthdemo/ui/RegisterActivity.kt
@@ -0,0 +1,16 @@
+package com.inspirecoding.firebaseauthdemo.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.inspirecoding.firebaseauthdemo.R
+
+class RegisterActivity : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+
+
+    }
+}
---
+++ app/src/main/java/com/inspirecoding/firebaseauthdemo/ui/SplashActivity.kt
@@ -0,0 +1,14 @@
+package com.inspirecoding.firebaseauthdemo.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.inspirecoding.firebaseauthdemo.R
+
+class SplashActivity : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
